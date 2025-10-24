23d9a96affb0d4248cd02fab3bf3b509757e030f
Bad64 <louvincent42@gmail.com>
2025-10-25 01:45:33 +0200

    Attempting a language selector

--- common/elements/langbar.js
+++ common/elements/langbar.js
@@ -1,8 +1,23 @@
-document.write('<div style="width: 100; display: flex; justify-content: right; padding-right: 1em;">');
+var url = window.location.href.split("/");
+url[0] += "/";
+var lang = url[3];
+
+document.write('<div id="langbar" style="width: 100; display: flex; justify-content: right; padding-right: 1em; margin-top: 5px">\n');
+document.write('<p id="langText"></p>')
+
+var locStrings = {
+    "en": {
+        "langSelector": "Choose your language:"
+    },
+    "fr": {
+        "langSelector": "Choisissez votre langue:"
+    }
+}
+//document.getElementById("langText").innerHTML = locStrings[lang]["langSelector"];
 
 function samePageDifferentLang() {
-    var url = window.location.href.split("/");
-    url[0] += "/";
+    
+
 
     var locales = [ "en", "fr" ];
     for (let locale of locales){
@@ -11,13 +26,13 @@ function samePageDifferentLang() {
         if ((url[3] == "fr") && (url[4] == "blog")) {   // Since I don't translate my blog
             document.write("<a href=\"" + url.slice(0,3).join("/") + "/fr/blog/1.shtml\">");
             document.write("<img src=\"/common/images/lang/" + locale + ".svg\"");
-            document.write("style=\"height: 18px; width: auto; cursor: pointer;\">");
+            document.write("style=\"width: auto; cursor: pointer;\">");
             document.write("</a>\n");
             document.write("<span>&nbsp;</span>");
         } else {
             document.write("<a href=\"" + url.join("/") + "\">");
             document.write("<img src=\"/common/images/lang/" + locale + ".svg\"");
-            document.write("style=\"height: 18px; width: auto; cursor: pointer;\">");
+            document.write("style=\"width: auto; cursor: pointer;\">");
             document.write("</a>\n");
             document.write("<span>&nbsp;</span>");
         }
@@ -26,4 +41,4 @@ function samePageDifferentLang() {
 }
 samePageDifferentLang();
 
-document.write("</div>");
+document.write("</div>\n");
